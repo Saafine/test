23f0434eab4aaedf47eb7563da85cec1e8e293a4
Pawel Lbs <pawel.lbs@gmail.com>
2019-09-05 18:49:05 +0200

    x

--- src/app/test2/test2.component.spec.ts
+++ src/app/test2/test2.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Test2Component } from './test2.component';
 import { Test2childModule } from './test2child/test2child.module';
+import { Test2childComponent } from './test2child/test2child.component';
 
 describe('Test2Component', () => {
   let component: Test2Component;
@@ -26,6 +27,15 @@ describe('Test2Component', () => {
   });
 
   it('should work', () => {
+    component.childRef = {
+      test: () => 'hey3'
+    } as Test2childComponent;
+    const spy = spyOn(component, 'parentChild');
+    component.ngAfterViewInit();
+    expect(spy).toHaveBeenCalledWith('hey3');
+  });
+
+  it('should work too', () => {
     const spy = spyOn(component, 'parentChild');
     component.ngAfterViewInit();
     expect(spy).toHaveBeenCalledWith('hey');
